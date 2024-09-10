f33ad630c626b87e4132a14ade6cec01d40d5fdb
marcos.almeida <me@marcosalmeida.dev>
2024-09-10 15:57:23 +0200

    my implementation

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateUI()
     }
 
-
+    func updateUI(){
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    @IBAction func selectedChoise(_ sender: UIButton) {
+        storyBrain.getNextStory(sender.currentTitle!)
+        
+        updateUI()
+    }
+    
 }
 
